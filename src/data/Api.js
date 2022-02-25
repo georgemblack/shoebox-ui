fb8f0725aecd34f5d8bbf016f525e6a739b3643d
George Black <contact@george.black>
2022-02-25 17:17:36 -0600

    Update API URL

--- src/data/Api.js
+++ src/data/Api.js
@@ -3,7 +3,7 @@ import { getAuthToken } from "../utils";
 let { API_URL } = process.env;
 
 export async function getEntriesAPI() {
-  const response = await fetch(`${API_URL}/entries`, {
+  const response = await fetch(`${API_URL}/api/entries`, {
     method: "GET",
     mode: "cors",
     headers: {
